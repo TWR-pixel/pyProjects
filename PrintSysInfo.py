884d00a0ef766561193dd8703cef3e0832080009
senya senya1 <46828715+senya-code@users.noreply.github.com>
2021-07-08 22:46:50 +0300

    Create PrintSysInfo.py

---
+++ PrintSysInfo.py
@@ -0,0 +1,28 @@
+import math
+import platform
+import os
+
+lis = [
+platform.processor(),
+platform.python_build(),
+platform.release(),
+platform.architecture(),
+platform.system(),
+platform.version()
+]
+
+#os.system("ipconfig")
+#pr = platform.processor()
+
+def printLis():
+    print()
+    print(">CPU stepping: " + lis[0])
+    print(">python: " + str(lis[1]))
+    print(">Выпуск системы: " + lis[2])
+    print(">Архитектура и версия ОС: " + str(lis[3]))
+    print(">Имя ОС: " + lis[4])
+    print(">Версия выпуска системы: " + lis[5] )
+    
+printLis()
+
+a = input()
