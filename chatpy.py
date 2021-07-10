70d7a6fb6ac882d0a59afa7d344d1515f22b723b
senya senya1 <46828715+senya-code@users.noreply.github.com>
2021-07-11 00:14:02 +0300

    Update chatpy.py

--- chatpy.py
+++ chatpy.py
@@ -1,34 +1,53 @@
 import socket
-
-type = input("Введите тип: ")
-
-if type == 's':
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("127.0.0.1", 2000))
-    server.listen()
-
-    user, adres = server.accept()
-    print("connect")
-
-    while True:
+import os
 
 
-        user.send(input().encode("utf-8"))
+def chat():
+    try:
 
-        data = user.recv(1024)
-        print(data.decode("utf-8"))
+        type = input("Введите тип: ")
+#server
+        if type == 's':
+
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.bind(("127.0.0.1", 2000))
+            server.listen()
+
+            user, adres = server.accept()
+            print("connect")
+
+            str = input()
+
+            while True:
+                user.send(str.encode("utf-8"))
+
+                data = user.recv(1024)
+                print(data.decode("utf-8"))
+#client
+        elif type == 'c':
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            client.connect(("127.0.0.1", 2000))
+
+            while True:
+                data = client.recv(1024)
+                print(data.decode("utf-8"))
+
+                client.send(input().encode("utf-8"))
+
+        else:
+            print("ERROR")
+            os.system("pause")
+
+    except:
+        print("ERROR")
+        os.system("pause")
 
 
-elif type == 'c':
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
+    chat()
 
-    client.connect(("127.0.0.1", 2000))
 
-    while True:
-        data = client.recv(1024)
-        print(data.decode("utf-8"))
-
-        client.send(input().encode("utf-8"))
+main()
 
 
